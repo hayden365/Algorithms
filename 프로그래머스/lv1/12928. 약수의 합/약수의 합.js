ae43e37f2afd2831aff301a00c1b477e74eaf0f5
Hyeji Kong <109419531+hayden365@users.noreply.github.com>
2023-04-11 23:16:53 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = 0;
+    
+    for (let i = 0; i <= n; i++){
+        if(n % i === 0){
+            answer+= i;
+        }
+    }
+    
+    return answer
+}
