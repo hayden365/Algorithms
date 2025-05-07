3f5b6f76dbbfbf4f58d30948654eeea3be166f44
Hyeji Kong <109419531+hayden365@users.noreply.github.com>
2025-05-07 21:31:29 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
@@ -1,9 +1,21 @@
 import Foundation
 
-func solution(_ n:Int) -> Int {
+func solution1(_ n:Int) -> Int {
     var answer = n / 7
     if 1...7 ~= n % 7 {
         answer += 1
     }
     return answer
+}
+
+func solution2(_ n: Int) -> Int {
+    var answer = n / 7
+    if (1...7).contains(n % 7) {
+        answer += 1
+    }
+    return answer
+}
+
+func solution(_ n: Int) -> Int {
+    return (n + 6) / 7
 }
